9e9dc0d6969d2d1fef304cc59e33accdd6e3b340
jerson@hotmail.com <jerson@hotmail.com>
2016-02-04 15:28:35 -0500

    Titulo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mi Blog' });
+  res.render('index', { title: 'No Click' });
 });
 
 module.exports = router;
