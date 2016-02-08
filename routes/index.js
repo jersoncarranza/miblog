ebe46f69cfe0d1caf3301a2da4c9ba7d98c137a9
jerson@hotmail.com <jerson@hotmail.com>
2016-02-07 23:44:30 -0500

    jade

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'No Click' });
+  res.render('index', { name: 'Bob' });
+  
 });
 
 module.exports = router;
